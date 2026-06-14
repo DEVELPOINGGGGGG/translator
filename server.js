d0f063645ab7bfae4f7582f2b848dc7e68ce527b
DEVELPOINGGGGGG <anilkumarsinghsingh397@gmail.com>
2026-06-14 12:16:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ async function processQueue() {
         // Launch highly optimized headless browser to save RAM (< 450MB)
        browser = await puppeteer.launch({
             headless: "new",
-            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome', // 🚀 ADD THIS LINE
+            executablePath: '/opt/render/project/src/.puppeteer_cache/chrome/linux-127.0.6533.88/chrome-linux64/chrome', // 🚀 ADD THIS LINE
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
